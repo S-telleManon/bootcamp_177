d5de8ee18b75914897c0ade4742a7ea66b6ce95e
S-telleManon <manonstelle888@gmail.com>
2025-08-28 22:37:29 +0400

    daily

---
+++ week7/day4/DailyChallenge/SQLPuzzle.sql
@@ -0,0 +1,10 @@
+SELECT * FROM FirstTab;
+SELECT * FROM SecondTab;
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL );
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 );
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab );
+SELECT COUNT(*) 
+FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL );
