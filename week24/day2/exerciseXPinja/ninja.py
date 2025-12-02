3dc6f5d787cfe1895bf892a54883a84ce5a3a2cf
S-telleManon <manonstelle888@gmail.com>
2025-12-02 18:27:32 +0400

    changes

--- week24/day2/exerciseXPinja/ninja.py
+++
@@ -1,9 +0,0 @@
-my_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-print(len(my_text))
-
-
-user_input= str(input("Please write the longest sentence without the character 'A': "))
-if "A".lower() not in user_input.lower():
-    print("Congratulationnnnssss ")
-else:
-    print("You failed ")
